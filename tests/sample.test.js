1e4f3b0c70a30f8eabf7f634d4c0299f7d9a4390
Uche Mukolo <uchemukolo@MacBook-Pro-99.local>
2018-09-24 15:58:36 +0100

    chore: Integrate TravisCI with readme badge
    - Install Mocha and Chai libraries
    - Configure travis.yml file
    - Write Dummy test
    - Add Markdown Badge link to the readMe file
    
    [Delivers #160452464]

---
+++ tests/sample.test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Dummy test',()=>{
+    it('should check if dummy test works', () => {
+        expect('Authors Haven').to.be.a('string');
+    })
+})
