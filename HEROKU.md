2c1094e2abffca2a62cad7200bdaf6ef32ae9f21
Olusola Oguntimehin <sola.dav7@gmail.com>
2018-09-27 17:23:56 +0100

    chore(Heroku): Setup and deploy Heroku pipeline
    - Create pipeline
    - Setup review apps (not yet activated)
    - Setup automatic deployment
    - Integrate Travis CI build to regulate Heroku deployments
    - Create HEROKU.md file for proper documentation
    - Refactor sequelize to use Database URL string
    - Configure review apps environment
    - Setup procfile to run migrations on successful releases
    - Fix error handler not returning json
    - Allow different URIs for development db and test db
    - Configure Travis postgres Database
    
    [Finishes 160452468]

---
+++ HEROKU.md
@@ -0,0 +1,15 @@
+## Pipline Design
+
+- Development > Review Apps > Staging > Production
+
+## Staging server
+
+- https://odin-ah-backend-staging.herokuapp.com
+
+## Production server
+
+- https://odin-ah-backend.herokuapp.com
+
+## Notes
+
+- Review apps have not been activated because the staging environment variables have not been setup
--- index.js
+++ index.js
@@ -3,15 +3,12 @@ import bodyParser from 'body-parser';
 import YAML from 'yamljs';
 import session from 'express-session';
 import cors from 'cors';
-import errorhandler from 'errorhandler';
 import morgan from 'morgan';
 import methodOverride from 'method-override';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
 
 const swaggerDocument = YAML.load('./swagger.yaml');
-const swaggerUi = require('swagger-ui-express');
-
-const isProduction = process.env.NODE_ENV === 'production';
 
 // Create global app object
 const app = express();
@@ -38,10 +35,6 @@ app.use(
   })
 );
 
-if (!isProduction) {
-  app.use(errorhandler());
-}
-
 app.use(routes);
 
 // / catch 404 and forward to error handler
@@ -51,27 +44,10 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// / error handlers
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use((err, req, res) => {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({
-      errors: {
-        message: err.message,
-        error: err
-      }
-    });
-  });
-}
-
 // production error handler
 // no stacktraces leaked to user
-app.use((err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     errors: {
