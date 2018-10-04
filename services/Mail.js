4c1b9fd21855cec22cd95df9ab1694523130c9a6
hammed noibi <hnobi>
2018-10-04 10:39:28 +0100

    get sendGrid token     create signup controller     create signup route     create signup validation middleware     write test for signup controller and middleware     create function to generate one time token     create a class for sending emails with sendGrid
        [delivers #160452475]

--- services/Mail.js
+++ services/Mail.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import sgMail from '@sendgrid/mail';
+import MailHelper from '../helpers/MailHelper';
 import emailMessages from './mailMessage';
 
 dotenv.config();
@@ -7,7 +8,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 /**
  * @exports Mail
- * @class AuthController
+ * @class Mail
  * @description Handles the user verification mail sending
  *
  */
@@ -23,9 +24,11 @@ class Mail {
   static sendVerification(req, res, next) {
     const { email, token } = req.user;
     const url = `http://localhost:3000/api/v1/auth/confirmation/${token}`;
-    const msg = emailMessages.signupVerification(email, url);
+    const subject = 'Confirmation Email';
+    const { message } = emailMessages.signupVerification(email, url);
+    const messageInfo = MailHelper.buildMessage(email, subject, message);
     sgMail
-      .send(msg)
+      .send(messageInfo)
       .then((resp) => {
         if (resp[0].statusCode === 202) {
           return res.status(201).send({
